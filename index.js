5b9ac18c48b418d2bebcd171d3f15f01f4177c6e
varad118 <72151241+varad118@users.noreply.github.com>
2025-01-14 22:44:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const {mineboty} = require("mineboty")
 mineboty();
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Bot has joined!')
 })
 
 app.listen(port, () => {
